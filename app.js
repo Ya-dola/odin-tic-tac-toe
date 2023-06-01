a27d0c66d753b171c6c32758f3433d4590240e8a
Yadola <Ya-dola@users.noreply.github.com>
2023-06-01 17:01:28 +0530

    Added Details Module

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
+// ENUMS
+const diffOptions = Object.freeze({
+                                      HUMAN: 'Human',
+                                      EASY: 'AI - Easy',
+                                      MEDIUM: 'AI - Medium',
+                                      HARD: 'AI - Hard'
+                                  });
 
+// MODULES
+const details = (() => {
+    const inputOne = document.getElementById('inputOne');
+    const inputTwo = document.getElementById('inputTwo');
+    const diffOne = document.getElementById('diffOne');
+    const diffTwo = document.getElementById('diffTwo');
+    const sbOne = document.getElementById('sbOne');
+    const sbTwo = document.getElementById('sbTwo');
+
+    let plyrOneName = "";
+    let plyrTwoName = "";
+    let diffOneCtr = 0; // Default = Human
+    let diffTwoCtr = 1; // Default = AI - Easy
+
+    function updateDiffOne() {
+        diffOne.textContent = Object.values(diffOptions)[diffOneCtr];
+        if (++diffOneCtr > 3) diffOneCtr = 0;
+    }
+
+    function updateDiffTwo() {
+        diffTwo.textContent = Object.values(diffOptions)[diffTwoCtr];
+        if (++diffTwoCtr > 3) diffTwoCtr = 0;
+    }
+
+    inputOne.addEventListener('blur', () => {
+        plyrOneName = inputOne.value !== '' ? inputOne.value : "";
+        sbOne.textContent = inputOne.value !== '' ? inputOne.value : "Player One";
+    });
+    inputTwo.addEventListener('blur', () => {
+        plyrTwoName = inputTwo.value !== '' ? inputTwo.value : "";
+        sbTwo.textContent = inputTwo.value !== '' ? inputTwo.value : "Player Two";
+    });
+    diffOne.addEventListener('click', updateDiffOne);
+    diffTwo.addEventListener('click', updateDiffTwo);
+
+    // Webpage Init Listener
+    document.addEventListener('DOMContentLoaded', () => {
+        updateDiffOne();
+        updateDiffTwo();
+    });
+
+    return {plyrOneName, plyrTwoName, diffOneCtr, diffTwoCtr};
+})();
+
+// FACTORIES
